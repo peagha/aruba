5eaf3436dcf3e28878167b675a887690438af5d3
Andrew Walter <andrew@xtrasimplicity.com>
2018-06-10 23:36:19 +1000

    CI: Windows - Fixed `check_for_exit_status` feature (Cucumber)

--- features/step_definitions/hooks.rb
+++ features/step_definitions/hooks.rb
@@ -90,6 +90,16 @@ Before '@requires-cat' do |scenario|
   end
 end
 
+Before '@requires-platform-windows' do |scenario|
+  next if FFI::Platform.windows?
+
+  if Cucumber::VERSION < '2'
+    scenario.skip_invoke!
+  else
+    skip_this_scenario
+  end
+end
+
 Before '@requires-posix-standard-tools' do |scenario|
   next unless Aruba.platform.which('printf').nil?
 
