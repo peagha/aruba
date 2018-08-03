0e43def88708454ce184020499885960e9abc2ef
Andrew Walter <andrew@xtrasimplicity.com>
2018-08-03 20:15:54 +1000

    CI: Fixed `#run_command` spec on Windows.

--- spec/aruba/api/commands_spec.rb
+++ spec/aruba/api/commands_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Aruba::Api::Commands do
   include_context 'uses aruba API'
 
   describe '#run_command' do
+    let(:cmd) { 'ruby -ne "puts $_"' }
+
     context 'when succesfully running a command' do
-      before(:each) { @aruba.run_command 'cat' }
+      before(:each) { @aruba.run_command cmd }
       after(:each) { @aruba.all_commands.each(&:stop) }
 
       it "respond to input" do
@@ -26,6 +28,15 @@ RSpec.describe Aruba::Api::Commands do
         @aruba.write_file(@file_name, "Hello\nWorld!")
         @aruba.pipe_in_file(@file_name)
         @aruba.close_input
+
+        @aruba.last_command_started.stop
+        last_command_output = @aruba.last_command_started.output
+
+        # Convert \r\n to \n, if present in the output
+        if last_command_output.include?("\r\n")
+          allow(@aruba.last_command_started).to receive(:output).and_return(last_command_output.gsub("\r\n", "\n"))
+        end
+
         expect(@aruba.last_command_started).to have_output "Hello\nWorld!"
       end
     end
@@ -40,7 +51,7 @@ RSpec.describe Aruba::Api::Commands do
       end
 
       it 'raises an error' do
-        expect { @aruba.run_command 'cat' }.to raise_error NotImplementedError
+        expect { @aruba.run_command cmd }.to raise_error NotImplementedError
       end
     end
   end
